8db032f8d7f48d2d59ff0e01a3c37ffd1ff0ba32
Diógenes Beserra <diogenesbeserra@gmail.com>
2022-05-11 16:41:40 -0300

    make it better

--- src/app/services/movimentacao.service.ts
+++ src/app/services/movimentacao.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const baseUrl: string = 'http://localhost:8080';
+const baseUrl: string = 'https://bankline-app-diogenes.herokuapp.com/';
 
 @Injectable({
   providedIn: 'root'
